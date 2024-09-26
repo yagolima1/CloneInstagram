54c31a258092b57edd45fc51086031355da06e83
Yago Gonçalves <138723627+yagolima1@users.noreply.github.com>
2024-09-26 17:53:26 -0300

    Add files via upload

--- src/componentes/ConteudoLateral.js
+++ src/componentes/ConteudoLateral.js
@@ -1,21 +1,36 @@
 import logo from '../imagens/Logo.png';
+import ListItem from './ListItem';
+import { FaHouse } from "react-icons/fa6";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FcSettings } from "react-icons/fc";
+import { FaHeart } from "react-icons/fa";
+import { IoChatbox } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { TbBrandSafari } from "react-icons/tb";
 
-export default function ConteudoLateral(){
-    return (
-        <div className='ConteudoLateral'>
-            <img src={logo} alt="logo" />
-        <div>Logo</div>
 
-        <div>Menu
-          <ul>
-            <li>Home</li>
-            <li>Pesquisa</li>
-            <li>Explorar</li>
-            <li>Mensagens</li>
-          </ul>
 
-        </div>
-        
+export default function ConteudoLateral() {
+  return (
+    <div className='ConteudoLateral'>
+      <img src={logo} className='logo' alt="logo" />
+
+
+      <div>
+        <ul className='menulateral'>
+
+        <ListItem texto='Home' icone= {<FaHouse/>}/>
+        <ListItem texto='Pesquisa' icone={<FaMagnifyingGlass />}/>
+        <ListItem texto='Explorer' icone={<TbBrandSafari />}/>
+        <ListItem texto='Mensagens' icone={<IoChatbox />}/>
+        <ListItem texto='Notificacaçao' icone={<FaHeart />}/>
+        <ListItem texto='Perfil' icone={<CgProfile />}/>
+        <ListItem texto='Mais' icone={<FcSettings />}/>
+      
+        </ul>
+
       </div>
-    )
+
+    </div>
+  )
 }
---
+++ src/componentes/ListItem.js
@@ -0,0 +1,6 @@
+export default function ListItem(props){
+    return (
+        <li>{props.icone} {props.texto}</li>
+       
+    )
+}
